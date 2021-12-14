a7232d39b2cceb15263aa8625eb79cf67825b2e2
unknown <m.kucha@wp.pl>
2021-12-14 20:28:25 +0100

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+class User:
+    pass
+
+
+class ChatRoom:
+    pass
+
+
+class Session:
+    pass
+
+
+class Message:
+    pass
+
+
+while True:
+    user = input("select user: ")
+
+    action = input("select action, 1-> ")
