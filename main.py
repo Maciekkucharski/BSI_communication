ab96f664a0fad5c90792f687a9fae1f377215bb5
unknown <m.kucha@wp.pl>
2021-12-15 17:16:28 +0100

    select problem

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from Crypto.PublicKey import ECC
 from Crypto import PublicKey
 
 CHATROOM_ITERATOR = 0
-USER_ITERATOR = 0
 
 encryption_dictionary = {
     "RSA": rsa.encrypt,
@@ -35,14 +34,15 @@ def encode_message(message: str, encoding: str, receiver_public_key: PublicKey) 
 
 
 class User:
-    def __init__(self):
-        global USER_ITERATOR
+    def __init__(self, name):
+        if name in user_dictionary:
+            print(f"such user already exists")
+            raise Exception("user already  exists")
         self.public_key_RSA, self.private_key_RSA = rsa.newkeys(512)
         self.private_key_ECC = ECC.generate(curve='P-256')
         self.public_key_ECC = self.private_key_ECC.public_key()
-        self.id = USER_ITERATOR
+        self.name = name
         self.messages_received = list()
-        USER_ITERATOR += 1
 
     def decode_message(self, message: bytes):
         decoded_message = rsa.decrypt(message, self.private_key_RSA)
@@ -53,27 +53,27 @@ class User:
 
     def show_messages(self):
         for message in self.messages_received:
-            print(f"'{self.decode_message(message.encoded_message)}':message from {message.sender}")
+            print(f"'{self.decode_message(message.encoded_message)}' :message from {message.sender.name}")
 
 
 def send_message(message: str, receiver: User, sender: User, encoding: str):
-    if receiver is None:
-        receiver = User()
-    user_dictionary[receiver.id] = receiver
+    user_dictionary[receiver.name] = receiver
     encoded_message = Message(encode_message(message, encoding, receiver.public_key_RSA), sender, encoding)
 
     receiver.receive_message(encoded_message)
 
 
 while True:
-    user = int(input("select user: "))
+    user = input("select user: ")
     if user not in user_dictionary:
-        user_dictionary[user] = User()
+        user_dictionary[user] = User(user)
     user_dictionary.get(user).show_messages()
-    action = input("select action, 1-> send message 2-> logout, 3-> exit")
+    action = input("1-> send message 2-> logout, 3-> exit")
     if action == '1':
-        message = input("what is the message")
-        receiver = int(input("who is the receiver, id()int"))
+        message = input("what is the message: ")
+        receiver = input("who is the receiver: ")
+        if receiver not in user_dictionary:
+            user_dictionary[receiver] = User(receiver)
         send_message(message, user_dictionary.get(receiver), user_dictionary.get(user), "RSA")
     elif action == '3':
         quit()
